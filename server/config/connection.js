3eda7f73652c64cb1e45b8a8c8c5f171ab9bceec
Carol Wargo <Carolwargo@gmail.com>
2023-08-13 09:39:46 -0400

    updated string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');   
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://carolwargo:!Newbeginnings2023@cluster0.jplhrrc.mongodb.net/');
 
 module.exports = mongoose.connection;

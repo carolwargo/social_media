bc98d649cd30eea03d773b199339ba25d2ddbcfc
Carol Wargo <Carolwargo@gmail.com>
2023-08-16 16:43:38 -0400

    fixing models and auth

--- server/models/post.js
+++ server/models/post.js
@@ -3,40 +3,40 @@ const Schema = mongoose.Schema;
 const dateFormat = require('../utils/dateFormat');
 
 const postSchema = new Schema(
-  {
-    userId: {
-      type: String,
-      required: true,
+    {
+      userId: {
+        type: String,
+        required: true,
+      },
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      location: String,
+      description: String,
+      picturePath: String,
+      userPicturePath: String,
+      likes: {
+        type: Map,
+        of: Boolean,
+      },
+      comments: {
+        type: Array,
+        default: [],
+      },
+      lastEditedAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
     },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    location: String,
-    bio: String,
-    picturePath: String,
-    userPicturePath: String,
-    likes: {
-      type: Map,
-      of: Boolean,
-    },
-    comments: {
-      type: Array,
-      default: [],
-    },
-
-     createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (timestamp) => dateFormat(timestamp),
-    }
-  },
-  { timestamps: true }
-
-);
-
-module.exports = mongoose.model("Post", postSchema);
+    { timestamps: true }
+  );
+  
+  const Post = mongoose.model("Post", postSchema);
+  
+  export default Post;
